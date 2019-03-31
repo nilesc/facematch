255c8bb6e2ba50b590d0c0e161ab1dea76397b1f
Niles Christensen <nnc2117@columbia.edu>
2019-03-31 11:14:30 -0400

    [Fixes] hound issues

--- setup_database.py
+++ setup_database.py
@@ -10,10 +10,12 @@ def adapt_array(arr):
     out.seek(0)
     return sqlite3.Binary(out.read())
 
+
 def convert_array(text):
     out = io.BytesIO(text)
     out.seek(0)
     return np.load(out)
+
 
 sqlite3.register_adapter(np.ndarray, adapt_array)
 sqlite3.register_converter("array", convert_array)
