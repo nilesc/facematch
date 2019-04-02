a0aa5896d38de23e7580e7b7dd434cc881e1d49c
Niles Christensen <nnc2117@columbia.edu>
2019-04-02 12:39:32 -0400

    [Changes] pose_estimator to combine several operations

--- pose_estimator.py
+++ pose_estimator.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torchvision
 from deep_head_pose.code.hopenet import Hopenet
 
+
 class PoseEstimator:
 
     def __init__(self, weights_path, gpu_available=False):
@@ -13,8 +14,8 @@ class PoseEstimator:
             map_location = 'gpu'
 
         self.model = Hopenet(torchvision.models.resnet.Bottleneck,
-                [3, 4, 6, 3],
-                66)
+                                [3, 4, 6, 3],
+                                66)
         self.model.load_state_dict(torch.load(weights_path,
             map_location=map_location))
 
@@ -24,21 +25,19 @@ class PoseEstimator:
         pitch = F.softmax(pitch, dim=1)
         roll = F.softmax(roll, dim=1)
 
+        outputs = torch.stack((yaw, pitch, roll), 1)
+
         index_tensor = torch.FloatTensor([index for index in range(66)])
-        index_tensor = index_tensor.view(1, -1).repeat(input_image.shape[0], 1)
+        index_tensor = index_tensor.view(1, 1, -1).repeat(input_image.shape[0], 3, 1)
 
-        yaw = torch.sum(yaw * index_tensor, 1) * 3 - 99
-        pitch = torch.sum(pitch * index_tensor, 1) * 3 - 99
-        roll = torch.sum(roll * index_tensor, 1) * 3 - 99
+        outputs = torch.sum(outputs * index_tensor, 2) * 3 - 99
 
-        return yaw, pitch, roll
+        return outputs
 
 
 if __name__ == '__main__':
     pe = PoseEstimator('pose_weights/hopenet_robust_alpha1.pkl',
-            gpu_available=False)
+                        gpu_available=False)
     random_inputs = torch.from_numpy(np.random.rand(10, 3, 200, 200))
-    yaw, pitch, roll = pe.estimate_pose(random_inputs)
-    print(yaw.data)
-    print(pitch.data)
-    print(roll.data)
+    outputs = pe.estimate_pose(random_inputs)
+    print(outputs)
