9707e4b94482c8f35a9ffe40c79a95466a40a15f
Krithika Kuppusamy <krithika@Krithikas-MacBook-Pro.local>
2019-04-17 17:55:05 -0400

    final

--- server.py
+++ server.py
@@ -1,20 +1,54 @@
-import os
+import os, subprocess
 from flask import Flask, request, render_template, g, redirect, Response
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
-@app.route('/userstudy.html')
+@app.route('/userstudy.html', methods=['GET', 'POST'])
 def user():
+	if request.method == 'POST':
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file:
+			filename = secure_filename(file.filename)
+			pathname = os.path.join('templates/uploads/', filename)
+			file.save(pathname)
+			global UPLOADED_FILE
+			UPLOADED_FILE = pathname
+			print(UPLOADED_FILE)
+			return redirect(request.url)
+
 	return render_template("userstudy.html")
 
-@app.route('/demo.html')
+@app.route('/demo.html', methods=['GET', 'POST'])
 def demo():
+	if request.method == 'POST':
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file:
+			filename = secure_filename(file.filename)
+			pathname = os.path.join('templates/uploads/', filename)
+			file.save(pathname)
+			global UPLOADED_FILE
+			UPLOADED_FILE = pathname
+			print(UPLOADED_FILE)
+			return redirect(request.url)
 	return render_template("demo.html")
 
 @app.route('/index.html')
@@ -23,11 +57,46 @@ def home():
 
 @app.route('/results.html')
 def res():
-	return render_template("results.html")
+	if UPLOADED_FILE:
+		proc = subprocess.Popen(['python3', 'find_match.py',  'video_database.db', '20170512-110547/20170512-110547.pb', 'hopenet_robust_alpha1.pkl', UPLOADED_FILE], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		out = proc.communicate()[0]
+		out = out.decode('ascii')
+		out = out.split('\n')
+		file_returned = out[1]
+		print(file_returned)
+		f_name = file_returned.split('/')[-1]
+		print("FILENAME:", f_name)
+		cmd = "cp " + file_returned + " static/" + f_name
+		print(cmd)
+		os.system(cmd)
+		file_returned = 'static/' + f_name
+
+		
+	return render_template("results.html", file_returned=file_returned)
 
 @app.route('/results-final.html')
 def res_final():
-	return render_template("results-final.html")
+	if UPLOADED_FILE:
+		proc = subprocess.Popen(['python3', 'find_match.py',  'video_database.db', '20170512-110547/20170512-110547.pb', 'hopenet_robust_alpha1.pkl', UPLOADED_FILE], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		out = proc.communicate()[0]
+		out = out.decode('ascii')
+		out = out.split('\n')
+		file_returned = out[1]
+		print(file_returned)
+		f_name = file_returned.split('/')[-1]
+		print("FILENAME:", f_name)
+		cmd = "cp " + file_returned + " static/" + f_name
+		print(cmd)
+		os.system(cmd)
+		file_returned = 'static/' + f_name
+	
+	return render_template("results-final.html", file_returned=file_returned)
+
+@app.route('/handle_data.html', methods=['POST'])
+def handle_data():
+	render_template("userstudy.html")
+	projectpath = request.form['file']
+	print(projectpath)
 
 
 if __name__ == "__main__":
